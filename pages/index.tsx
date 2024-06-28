87e28a8a2bf8bc9a6cab789fdd18d55e07d0e252
Kunal Gursal <kunalgursal2923@gmail.com>
2024-06-28 23:36:09 +0530

    update feedcard

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,11 +83,6 @@ export default function Home() {
         <div className="col-span-6 border-l-[1px] border-r-[1px] border-gray-800 h-screen overflow-scroll">
           <FeedCard />
           <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
           
         </div>
         <div className="col-span-3 p-5">
